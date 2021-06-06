5b7a420b6e6ea0e2f14a8c93bd19ea77be953952
Nilesh <nileshtayade666@gmail.com>
2021-06-06 22:15:54 +0530

    first commit

---
+++ src/main/java/com/tathasthu/first/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.tathasthu.first.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	
+	@GetMapping("/")
+	public String getIndex()
+	{
+		return "index";
+	}
+	
+	@GetMapping("/register")
+	public String getRegister()
+	{
+		return "register";
+	}
+
+}
